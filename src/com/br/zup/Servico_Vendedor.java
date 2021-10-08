9f2b10760b214e0381f7484bec2f7658fa00e933
Let Marçal <leticiamarcalmatias@gmail.com>
2021-10-08 11:44:00 -0300

    Criar método para cadastrar vendedores

--- src/com/br/zup/Servico_Vendedor.java
+++ src/com/br/zup/Servico_Vendedor.java
@@ -1,4 +1,17 @@
 package com.br.zup;
 
-public class Servico_Funcionario {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Servico_Vendedor {
+    //-----------------------------------Atributo responsável por criar a lista de vendedores
+    private static List<Vendedor> vendedores = new ArrayList<>();
+
+    //-----------------------------------MÉTODO PARA CADASTRAR UM VENDEDOR
+    public static Vendedor cadastrarVendedor(String nome, String matricula, String email){
+        Vendedor vendedor = new Vendedor(nome, matricula, email);
+        vendedores.add(vendedor);
+
+        return vendedor;
+    }
 }
