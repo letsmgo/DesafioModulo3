ff4c415acf8675f4034a02245894fef0f9f5ab44
Let Marçal <leticiamarcalmatias@gmail.com>
2021-10-08 15:30:03 -0300

    Criar método para cadastrar venda

--- src/com/br/zup/Servico_Venda.java
+++ src/com/br/zup/Servico_Venda.java
@@ -1,4 +1,25 @@
 package com.br.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Servico_Venda {
+    //-----------------------------------Atributo responsável por criar a lista de clientes
+    private static List<Venda> vendas = new ArrayList<>();
+
+    //-----------------------------------MÉTODO PARA CADASTRAR UMA VENDA
+    public static Venda cadastrarVenda(String cpfCliente, String emailVendedor, double valorASerPago, String dataDeRegistro) throws Exception{
+        Cliente cliente =  Servico_Cliente.buscarClientePeloEmail(cpfCliente);
+        Vendedor vendedor = Servico_Vendedor.validarVendedor(emailVendedor);
+        Servico_Vendedor.validarVendedor(emailVendedor);
+        Venda venda = new Venda(cliente, vendedor, valorASerPago, dataDeRegistro);
+        vendas.add(venda);
+
+        return venda;
+    }
+
+
+
+
+
 }
