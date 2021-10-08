3f9f20b76ab3d9aa5f07d330f1049a6d27a4b83f
Let Marçal <leticiamarcalmatias@gmail.com>
2021-10-08 11:10:37 -0300

    Cadastrar um cliente

--- src/com/br/zup/Servico_Cliente.java
+++ src/com/br/zup/Servico_Cliente.java
@@ -1,4 +1,18 @@
 package com.br.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Servico_Cliente {
+    //-----------------------------------Atributo responsável por criar a lista de clientes
+    private static List<Cliente> clientes = new ArrayList<>();
+
+    //-----------------------------------MÉTODO PARA CADASTRAR UM CLIENTE
+    public static Cliente cadastrarCliente(String nome, String cpf, String email){
+        Cliente cliente = new Cliente(nome, cpf, email);
+         clientes.add(cliente);
+
+         return cliente;
+    }
+
 }
